3109752da79d0e3fa0606ec755031f73e35800c4
SonOfStone <rwbrinson3@gmail.com>
2018-11-21 11:30:56 -0600

    Bot with youtube playing capabilities

---
+++ my_bot.js
@@ -0,0 +1,117 @@
+//initialize bot
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+//Queue of songs
+var songQueue = []
+
+client.on("ready", () => {
+	client.user.setActivity("Online!");
+	console.log('I am ready!');
+})
+
+client.on('message', (receivedMessage) => {
+	if (receivedMessage.author == client.user){
+		// prevent bot from responding to itself
+		return
+	}
+	if (receivedMessage.content.startsWith("!!!")){
+		processCommand(receivedMessage)
+	}
+})
+
+//function to process incoming command
+function processCommand(receivedMessage){
+	// remove leading "!!!"
+	let fullCommand = receivedMessage.content.substr(3)
+	// split command into parts
+	let splitCommand = fullCommand.split(" ")
+	// grab main command
+	let primaryCommand = splitCommand[0]
+	// grab remaining arguments
+	let arguments = splitCommand.slice(1)
+	
+	console.log("Command received: " + primaryCommand)
+	console.log("Arguments: " + arguments)
+	
+	// redirect commands to other functions
+	if(primaryCommand == "help"){
+		helpCommand(arguments, receivedMessage)
+	}else if(primaryCommand == "play"){
+		playCommand(arguments, receivedMessage)
+	}else if(primaryCommand == "pause"){
+		pauseCommand(arguments, receivedMessage)
+	}else if(primaryCommand == "resume"){
+		resumeCommand(arguments, receivedMessage)
+	}else if(primaryCommand == "stop"){
+		stopCommand(arguments, receivedMessage)
+	}else{
+		receivedMessage.channel.send("Unknown Command")
+	}
+}
+
+//help command function
+function helpCommand(arguments, receivedMessage){
+	if(arguments.length > 0){
+		receivedMessage.channel.send("It looks like you might need help with " + arguments)
+	} else {
+		receivedMessage.channel.send("I'm not sure what you need help with.")
+	}
+}
+
+function playCommand(arguments, receivedMessage){
+	if(arguments.length == 1){
+		console.log("pushing " + arguments[0])
+		songQueue.push(arguments[0])
+	}else{
+		receivedMessage.channel.send("Please provide a link")
+		return
+	}
+	if(!receivedMessage.member.voiceChannel){
+		receivedMessage.channel.send("You must be in a voice channel")
+		return
+	}
+	if(!receivedMessage.guild.voiceConnection) receivedMessage.member.voiceChannel.join().then(function(connection){
+		play(connection, receivedMessage)
+	})
+	.catch(console.error)
+}
+
+function play(connection, receivedMessage){
+	console.log("starting play function")
+	const ytdl = require('ytdl-core')
+	const streamOptions = { seek: 0, volume: .08 }
+	const stream = ytdl(songQueue[0], {filter: "audioonly"})
+	dispatcher = connection.playStream(stream, streamOptions)
+	songQueue.shift();
+	dispatcher.on("end", () =>{
+		if(songQueue[0]){
+			play(connection, receivedMessage);
+		}else{
+			connection.disconnect();
+		}
+	})
+}
+
+//pauses current music
+function pauseCommand(arguments, receivedMessage){
+	var server = servers[message.guild.id]
+	if(server.dispatcher){
+		server.dispatcher.pause()
+	}
+}
+
+//resumes current music
+function resumeCommand(arguments, receivedMessage){
+	broadcast.resume()
+}
+
+//ends current song
+function stopCommand(arguments, receivedMessage){
+	if(receivedMessage.guild.voiceConnection){
+		receivedMessage.member.voiceChannel.leave()
+	}
+}
+
+bot_secret_token = "NTE0NjgxNTM1OTE2NjcwOTc2.DtaJhw.dFzJKR6qlJiedTwZ61qdhrsqpYg"
+client.login(bot_secret_token)
---
+++ my_botv2.js
@@ -0,0 +1,126 @@
+//initialize bot
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+//Queue of songs
+var broadcast = {}
+var songQueue = []
+
+client.on("ready", () => {
+	client.user.setActivity("Online!");
+	console.log('I am ready!');
+})
+
+client.on('message', (receivedMessage) => {
+	if (receivedMessage.author == client.user){
+		// prevent bot from responding to itself
+		return
+	}
+	if (receivedMessage.content.startsWith("!!!")){
+		processCommand(receivedMessage)
+	}
+})
+
+//function to process incoming command
+function processCommand(receivedMessage){
+	// remove leading "!!!"
+	let fullCommand = receivedMessage.content.substr(3)
+	// split command into parts
+	let splitCommand = fullCommand.split(" ")
+	// grab main command
+	let primaryCommand = splitCommand[0]
+	// grab remaining arguments
+	let arguments = splitCommand.slice(1)
+	
+	console.log("Command received: " + primaryCommand)
+	console.log("Arguments: " + arguments)
+	
+	// redirect commands to other functions
+	if(primaryCommand == "help"){
+		helpCommand(arguments, receivedMessage)
+	}else if(primaryCommand == "play"){
+		playCommand(arguments, receivedMessage)
+	}else if(primaryCommand == "pause"){
+		pauseCommand(arguments, receivedMessage)
+	}else if(primaryCommand == "resume"){
+		resumeCommand(arguments, receivedMessage)
+	}else if(primaryCommand == "skip"){
+		skipCommand(arguments, receivedMessage)
+	}else if(primaryCommand == "stop"){
+		stopCommand(arguments, receivedMessage)
+	}else{
+		receivedMessage.channel.send("Unknown Command")
+	}
+}
+
+//help command function
+function helpCommand(arguments, receivedMessage){
+	if(arguments.length > 0){
+		receivedMessage.channel.send("It looks like you might need help with " + arguments)
+	} else {
+		receivedMessage.channel.send("I'm not sure what you need help with.")
+	}
+}
+
+//pushes songs to queue
+function playCommand(arguments, receivedMessage){
+	if(arguments.length != 1){
+		receivedMessage.channel.send("Please provide a link")
+		return
+	}
+	if(!receivedMessage.member.voiceChannel){
+		receivedMessage.channel.send("You must be in a voice channel")
+		return
+	}
+	console.log("pushing " + arguments[0])
+	songQueue.push(arguments[0])
+	if(!receivedMessage.guild.voiceConnection) receivedMessage.member.voiceChannel.join().then(function(connection){
+		play(connection, receivedMessage)
+	})
+	.catch(console.error)
+}
+
+function play(connection, receivedMessage){
+	console.log("starting play function")
+	const ytdl = require('ytdl-core')
+	const streamOptions = { seek: 0, volume: .08 }
+	const stream = ytdl(songQueue[0], {filter: "audioonly"})
+	broadcast = client.createVoiceBroadcast();
+	broadcast.playStream(stream, streamOptions)
+	const dispatcher = connection.playBroadcast(broadcast)
+	songQueue.shift();
+	broadcast.on("end", () =>{
+		broadcast.destroy()
+		})
+	dispatcher.on("end", () =>{
+		if(songQueue[0]){
+			play(connection, receivedMessage);
+		}else{
+			connection.disconnect();
+		}
+	})
+}
+
+//pauses current music
+function pauseCommand(arguments, receivedMessage){
+	broadcast.pause()
+}
+
+//resumes current music
+function resumeCommand(arguments, receivedMessage){
+	broadcast.resume()
+}
+
+//skips current song
+function skipCommand(arguments, receivedMessage){
+	broadcast.destroy()
+}
+
+//ends entire queue
+function stopCommand(arguments, receivedMessage){
+	songQueue = []
+	broadcast.destroy()
+}
+
+bot_secret_token = "NTE0NjgxNTM1OTE2NjcwOTc2.DtaJhw.dFzJKR6qlJiedTwZ61qdhrsqpYg"
+client.login(bot_secret_token)
