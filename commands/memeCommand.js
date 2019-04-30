cee0eeb2ccacbb804bd94f5cbce9c88aaf488ef3
Wayne Brinson <rwbrinson3@gmail.com>
2019-04-30 16:31:50 -0500

    added meme command which sends a meme to the user

---
+++ commands/memeCommand.js
@@ -0,0 +1,20 @@
+module.exports = {
+	name: 'meme',
+	description: 'Returns a meme from reddit',
+	execute(receivedMessage, arguments, client) {
+        const sf = require('snekfetch')
+        const Discord = require('discord.js')
+
+        sf.get(`https://www.reddit.com/r/memes/random.json?limit=1`).then(res => {
+            console.log(res.body[0].data.children[0].data.url)
+            const newEmbed = new Discord.RichEmbed()
+                .setColor("DARK_BLUE")
+                .setTitle(res.body[0].data.children[0].data.title)
+                .setURL("https://reddit.com/" + res.body[0].data.children[0].data.permalink)
+                .setImage(res.body[0].data.children[0].data.url)
+            receivedMessage.channel.send(newEmbed)
+            //receivedMessage.channel.send(res.body[0].data.children[0].data.url)
+
+        });
+	},
+};
