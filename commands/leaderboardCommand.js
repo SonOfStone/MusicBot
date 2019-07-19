65b5188f577062e48e0ce39a187b00affe504199
SonOfStone <rwbrinson3@gmail.com>
2019-07-18 23:30:02 -0500

    added leaderboard command

---
+++ commands/leaderboardCommand.js
@@ -0,0 +1,27 @@
+module.exports = {
+	name: 'leaderboard',
+	description: 'Lists the top 5 biggest losers',
+	execute(receivedMessage, arguments, client) {
+        //grab a list of members in the discord server
+        var members = receivedMessage.guild.members.array();
+        
+        //initialize json
+        var leaders = {};
+
+        //get scores for all members
+        for(var member in members){
+            var personal_score = client.helpers.get("getScore").execute(members[member], client);
+            leaders[personal_score] = members[member]
+        }
+                
+        //use the scores as keys and grab the last 5
+        scores = Object.keys(leaders)
+        var outputStr = "Biggest Losers: "
+        for(var i=1; i < 6 && i < scores.length; i++){
+            const score = scores[scores.length-i];
+            const displayName = leaders[score].displayName
+            outputStr += "\n        " + displayName + " : " + score;
+        }
+        receivedMessage.channel.send(outputStr);
+    }
+};
