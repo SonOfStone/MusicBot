17791f2f2bdf70379df1b5773e73f849d296b1ac
SonOfStone <rwbrinson3@gmail.com>
2020-05-28 18:05:06 -0400

    added randomsong autoplay and edited help

--- commands/autoPlayCommand.js
+++ commands/autoPlayCommand.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: 'autoplay',
-	description: 'Turns on autoplay, and if supplied with a link plays that song also',
+    description: 'Turns on and off autoplay. Defaults the type to recommended but can specify the type to random. If supplied with a link plays that song and grabs recommended ones',
+    example: ", {prefix}autoplay random, {prefix}autoplay Never gonna give you up",
 	execute(receivedMessage, arguments, client) {
         variables = client.variables
         helpers = client.helpers
@@ -9,7 +10,7 @@ module.exports = {
         if(variables.has("autoPlayFlag" + receivedMessage.guild.id)){
             autoPlayFlag = variables.get("autoPlayFlag" + receivedMessage.guild.id)
         }else{
-            variables.set("autoPlayFlag" + receivedMessage.guild.id, false)
+            variables.set("autoPlayFlag" + receivedMessage.guild.id, "off")
             autoPlayFlag = variables.get("autoPlayFlag" + receivedMessage.guild.id)
         }
         
@@ -21,24 +22,33 @@ module.exports = {
             songQueue = variables.get("songQueue" + receivedMessage.guild.id)
         }
         
-        //if supplied an argument turn autoplay flag on and play that argument
+        //if supplied an argument turn autoplay flag to recommended and play that argument
         if(arguments.length>=1){
-            variables.set("autoPlayFlag" + receivedMessage.guild.id, true)
-            client.commands.get("play").execute(receivedMessage, arguments, client)
-        }else if(arguments.length==0){
-            //make sure autoPlayFlag is defined
-            if(autoPlayFlag === true || autoPlayFlag === false){
-                autoPlayFlag = !autoPlayFlag
-            //if not defined make true
+            //check if argument is the literal "random"
+            if(arguments[0].toLowerCase() == "random"){
+                receivedMessage.channel.send("Autoplay is on random.")
+                variables.set("autoPlayFlag" + receivedMessage.guild.id, "random");
+                client.commands.get("randomsong").execute(receivedMessage, [], client);
+            //else find the song
             }else{
-                autoPlayFlag = true
+                variables.set("autoPlayFlag" + receivedMessage.guild.id, "recommended");
+                client.commands.get("play").execute(receivedMessage, arguments, client);
+            }
+        //if no argument
+        }else if(arguments.length==0){
+            //toggle to recommended if autoplay is off or undefined
+            if(autoPlayFlag === undefined || autoPlayFlag === "off" || autoPlayFlag == "random"){
+                autoPlayFlag = "recommended";
+            //if it is on another type of autoplay turn it off
+            }else{
+                autoPlayFlag = "off"
             }
             variables.set("autoPlayFlag" + receivedMessage.guild.id, autoPlayFlag)
             //if autoplay is on start autoplaying
-            if(autoPlayFlag){
-                receivedMessage.channel.send("Autoplay is on.")
+            if(autoPlayFlag == "recommended"){
+                receivedMessage.channel.send("Autoplay is on recommended.")
                 lastSong = variables.get("lastSong" + receivedMessage.guild.id)
-                if(autoPlayFlag && songQueue.length == 0 && lastSong !== undefined){
+                if(songQueue.length == 0 && lastSong !== undefined){
                     console.log(lastSong)
                     //trying to find next song
                     console.log("finding next song")
--- commands/randomSongCommand.js
+++ commands/randomSongCommand.js
@@ -12,8 +12,9 @@ module.exports = {
         
         const rp = require('request-promise')
         const cheerio = require('cheerio')
-        var videoNum = Math.floor((Math.random() * 1887))
-        const url = 'https://ytroulette.com/?i=' + videoNum + '&c=1'
+        //picks between 6000 songs on ytroulette I may be able to just set to a random number and the website will give me a random song
+        var videoNum = Math.floor((Math.random() * 5695))
+        const url = 'https://ytroulette.com/?i=' + videoNum + '&c=1&l=en'
         
         rp(url)
             .then(function(html){
@@ -21,11 +22,13 @@ module.exports = {
                 const $ = cheerio.load(html)
                 var videoTitle = $("head > title").text()
                 
-                var apiUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + videoTitle + "&type=video&key=" + client.variables.get("Api_key")
+                var apiUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=lyrics " + videoTitle + "&type=video&key=" + client.variables.get("Api_key")
                 var apiCallResponse = helpers.get("httpGetAsync").execute(apiUrl, helpers.get("searchVideoResponseHandler"), receivedMessage, client)
             })
             .catch(function(err){
                 //handle error
+                console.log("Encountered an issue loading the random song");
+                console.log(err);
             });
 	},
 };
